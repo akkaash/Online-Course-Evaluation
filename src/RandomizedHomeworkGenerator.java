439c411a0e59ddfa76cbf792e3b805b6bd58aff7
Akkaash Goel <agoel3@ncsu.edu>
2014-10-20 11:26:31 -0400

    Added servlet for homework generator Added constants and create connection class so that we can get the connection simply by creating an object of CreateConnection class.

---
+++ src/RandomizedHomeworkGenerator.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author akkaashgoel
+ *
+ */
+public class RandomizedHomeworkGenerator {
+	
+	private int homeworkNumber;
+
+	public RandomizedHomeworkGenerator(int homeworkNumber) {
+		super();
+		this.homeworkNumber = homeworkNumber;
+	}
+
+	public int getHomeworkNumber() {
+		return homeworkNumber;
+	}
+
+	public void setHomeworkNumber(int homeworkNumber) {
+		this.homeworkNumber = homeworkNumber;
+	}
+
+	public void getHomework() {
+		
+		
+		
+	}
+	
+	
+
+}
