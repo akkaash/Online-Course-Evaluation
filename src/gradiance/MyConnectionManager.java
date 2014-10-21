d49c11be38d26f4a591234e0d963745df400da45
Akkaash Goel <agoel3@ncsu.edu>
2014-10-21 01:45:09 -0400

    Refactored Homework.java and MyConnectionManager.java

---
+++ src/gradiance/MyConnectionManager.java
@@ -0,0 +1,34 @@
+package gradiance;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import oracle.jdbc.OracleDriver;
+import oracle.jdbc.driver.OracleConnection;
+
+
+public class MyConnectionManager {
+	
+	private Connection connection;
+	
+	public MyConnectionManager() {
+		try {
+			Class.forName(MyConstants.oracleDriverName);
+			this.connection = (OracleConnection) DriverManager.getConnection(MyConstants.connectionUrl, MyConstants.oracleUsername, MyConstants.oraclePassword);
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	
+
+}
