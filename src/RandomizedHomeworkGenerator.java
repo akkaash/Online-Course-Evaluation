c12239c636b395ba0e7797c149d4ff3297989241
agoel3 <agoel3@ncsu.edu>
2014-10-25 17:50:02 -0400

    Update RandomizedHomeworkGenerator.java

--- src/RandomizedHomeworkGenerator.java
+++ src/RandomizedHomeworkGenerator.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import oracle.jdbc.OraclePreparedStatement;
+
 /**
  * 
  */
@@ -8,27 +15,51 @@
  */
 public class RandomizedHomeworkGenerator {
 	
-	private int homeworkNumber;
+	private Homework homework;
+	private Connection connection;
 
-	public RandomizedHomeworkGenerator(int homeworkNumber) {
+	public RandomizedHomeworkGenerator(Connection connection, Homework homework) {
 		super();
-		this.homeworkNumber = homeworkNumber;
+		this.homework = homework;
+		this.connection = connection;
 	}
 
-	public int getHomeworkNumber() {
-		return homeworkNumber;
-	}
-
-	public void setHomeworkNumber(int homeworkNumber) {
-		this.homeworkNumber = homeworkNumber;
-	}
-
-	public void getHomework() {
-		
-		
-		
-	}
+//	public int getHomeworkNumber() {
+//		return homeworkNumber;
+//	}
+//
+//	public void setHomeworkNumber(int homeworkNumber) {
+//		this.homeworkNumber = homeworkNumber;
+//	}
 	
+	private ResultSet homeworkResultSet = null;
+	
+	public ResultSet getQuestions(){
+		
+		String randomQuestionString = "SELECT * FROM (SELECT * FROM " + MyConstants.QUESTIONS_TABLE_NAME +""
+				+ 		" ORDER BY dbms_random.value) t WHERE"+ ""
+						+"t." + MyConstants.QUESTIONS_COLS[5] + " >= ?" + ""
+				+"and "	+"t." + MyConstants.QUESTIONS_COLS[5] + " <= ?" + ""
+				+"and " +"t." + MyConstants.QUESTIONS_COLS[1] + " = ?" + ""
+				+"and " +"rownum < ? ;";
+		try {
+			OraclePreparedStatement stmt = (OraclePreparedStatement) connection.prepareStatement(randomQuestionString);
+			
+			stmt.setInt(1, homework.getDifficulty_level_start());
+			stmt.setInt(2, homework.getDifficulty_level_end());
+			stmt.setInt(3, homework.getChapter_id());
+			stmt.setInt(4, homework.getNumberOfQuestions());
+			
+			ResultSet resultSet = stmt.executeQuery();
+			return resultSet;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+		
 	
 
 }
